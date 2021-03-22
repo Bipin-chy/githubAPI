129d1467d3e84c77ebc1cbb2d2b9d546a4512560
Bipinchaudhary <57010268+Bipin-chy@users.noreply.github.com>
2021-03-22 13:36:57 +0545

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ fetch("https://api.github.com/users/Bipin-chy/repos")
        
         const tableBody = document.createElement('tbody');
         const tableRow = document.createElement('tr');
-        //  const urlName = 'https://github.com/Bipin-chy/';
+//          const urlName = 'https://github.com/Bipin-chy/';
 
 
         tableBody.appendChild(tableRow);
 
         tableRow.innerHTML = `<td>${item.id}</td>
         <td>${item.name}</td>
-        <td><a href="${item.owner.htm_url + '/' + item.name}">${item.owner.htm_url + '/' + item.name}</a></td>
-        <td><a href="${item.owner.htm_url + '/' + item.name + '/'}">${item.name}</a></td>
+        <td><a href="${item.htm_url + '/' + item.name}">${item.htm_url + '/' + item.name}</a></td>
+        <td><a href="${item.htm_url + '/' + item.name + '/'}">${item.name}</a></td>
         `;
         table.appendChild(tableBody);
     })
