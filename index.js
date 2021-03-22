652da145ce13a4cb31cca2d99cb3bec094d02ef2
Bipinchaudhary <57010268+Bipin-chy@users.noreply.github.com>
2021-03-22 13:52:22 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const table = document.querySelector('.GeneratedTable');
 
+
+
 fetch("https://api.github.com/users/Bipin-chy/repos")
 .then(res=>res.json()).then(data=>{
     data.map((item)=>{
-        
+        data.sort();
 
         console.log(item);
         
